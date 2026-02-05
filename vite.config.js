bb16a6be50b0a6f780e90c3fc1f4f917f569c3d0
Leo Oladimu <leo@oleo.dev>
2026-02-05 02:44:51 -0800

    cleaned up pics

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-/* eslint-env node */
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'node:url'
-import { dirname } from 'node:path'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const root = dirname(fileURLToPath(import.meta.url))
-  const env = loadEnv(mode, root, '')
-
-  return {
-    plugins: [react()],
-    base: env.VITE_BASE_URL || '/',
-  }
+export default defineConfig({
+  plugins: [react()],
 })
